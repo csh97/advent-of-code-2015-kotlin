fe7c60720139b80592ee81674eb501f8e9718e37
Conor Haworth <conorhaworth@mac.home>
2024-11-26 20:48:22 +0000

    Day 01 - part 2

--- src/day01.txt.kt
+++ src/day01.txt.kt
@@ -8,7 +8,19 @@ fun part1(input: String): Int {
     }
 }
 
+fun part2(input: String): Int {
+    return input.toCharArray().fold(0 to 0) { (floor, index), char ->
+        if (floor == -1) return@part2 index
+        when(char) {
+            '(' -> (floor + 1) to index + 1
+            ')' -> floor - 1 to index + 1
+            else -> floor to index + 1
+        }
+    }.first
+}
+
 fun main() {
     val input = "day01.txt".readFile()
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
